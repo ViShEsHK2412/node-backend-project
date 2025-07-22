ea736485069cd1e574693881299c84f3500038e0
VisheshKathuria <vishesh.kathuria2004@mail.com>
2025-07-22 23:06:35 +0530

    add use and video model

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,7 +1,7 @@
 class ApiError extends Error {
     constructor(
         statusCode,
-        message="Somwthing went wrong",
+        message="Something went wrong",
         errors = [],
         stack=""
     ){
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -10,14 +10,14 @@ export {asyncHandler}
 
 
 
-
-
+//both of the codes are correct they are jsut different syntaxes 
+//Yaha par asyncHandler jo use kra h vo function k andar function h usko desructure krke likha h it is basically (func)=>{()=>{}}
 // const asyncHandler = (fn)=>async(req,res,next)=>{
 //     try{
 //         await fn(req,res,next)
 //     }catch(error){
 //         res.status(err.code || 500).json({
-//             sucess: false,
+//             success: false,
 //             message:err.message
 //         })
 //     }
